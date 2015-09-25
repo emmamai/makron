f381d5a1b0da04b7ee1d004454bc548dbbe321c1
_em <em@emmamai.xyz>
2015-09-24 22:38:09 -0400

    Fixed leakiness.
    My little 47 lines of code now runs valgrind-clean.
    What an accomplishment!

--- main.c
+++ main.c
@@ -11,6 +11,7 @@ int BecomeWM( xcb_connection_t *c, xcb_screen_t *screen ) {
 	cookie = xcb_change_window_attributes_checked( c, screen->root, XCB_CW_EVENT_MASK, v );
 	error = xcb_request_check( c, cookie );
 	if ( error ) {
+		free( error );
  		return -1;
 	}
 	return 0;
@@ -25,26 +26,24 @@ int main() {
 	if ( xcb_connection_has_error( c ) ) {
 			printf( "Uh oh! It looks like X isn't running.\n" );
 			printf( "You'll need to start it before you can run toolwm.\n" );
+			xcb_disconnect( c );
 			return 1;
 	}
-
 	screen = xcb_setup_roots_iterator( xcb_get_setup( c ) ).data;
-
-	
 	if ( BecomeWM( c, screen ) < 0 ) {
 		printf( "Uh oh! It looks like there's another window manager running.\n" );
 		printf( "You'll need to close it before you can run toolwm.\n" );
+		//free( screen );
+		xcb_disconnect( c );
 		return 1;
 	}
-
 	while( ( e = xcb_wait_for_event( c ) ) != NULL ) {
 		printf( "Hey, look, an event! I wonder what it is. I sure don't know! I haven't been programmed to handle it yet!\n" );
 
 		free( e );
 	}
-	
 	printf( "Looks like we're done here. See you next time!\n" );
+	//free( screen );
 	xcb_disconnect( c );
-
 	return 0;
 }
