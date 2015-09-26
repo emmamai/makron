be983e1f89167a44e0627f38bce04b8b128c8d92
_em <em@emmamai.xyz>
2015-09-26 16:27:30 -0400

    Changed window creation handling
    Moved x/y configuration into its own function, ConfigureWindow.
    DoCreateNotify doesn't configure the frame size.
    DoReparentNotify configures the frame size when called.
    Added DoConfigureRequest, which passes appropriate arguments to ConfigureWindow.
    
    This fixes issues with xterm, as well as any other programs that expect window
    configuration to work. Which is probably most of them.

--- main.c
+++ main.c
@@ -3,7 +3,20 @@
 #include <signal.h>
 #include <xcb/xcb.h>
 
+#define PROGRAM_NAME "makron"
+
+#define VERSION_MAJOR 0
+#define VERSION_MINOR 1
+#define VERSION_STRING "0.1"
+#define VERSION_BUILDSTR "13"
+
 #define MAX_CLIENTS 1024
+
+#define BORDER_SIZE_LEFT 1
+#define BORDER_SIZE_RIGHT 1
+#define BORDER_SIZE_TOP 19
+#define BORDER_SIZE_BOTTOM 1
+
 
 xcb_connection_t *c;
 xcb_screen_t *screen;
@@ -18,9 +31,9 @@ typedef enum {
 } clientWindowState_t;
 
 typedef enum {
-	STATE_UNMANAGED, //override redirect
-	STATE_REPARENTING,
-	STATE_MANAGED,
+	STATE_NO_REDIRECT, //override redirect
+	STATE_INIT,
+	STATE_REPARENTED,
 	STATE_TRANSIENT
 } clientManagementState_t;
 
@@ -43,12 +56,59 @@ typedef struct client_s {
 
 client_t *firstClient;
 
+void ConfigureClient( client_t *n, short x, short y, unsigned short width, unsigned short height ) {
+	unsigned short pmask = 	XCB_CONFIG_WINDOW_X |
+							XCB_CONFIG_WINDOW_Y |
+							XCB_CONFIG_WINDOW_WIDTH |
+							XCB_CONFIG_WINDOW_HEIGHT |
+							XCB_CONFIG_WINDOW_BORDER_WIDTH;
+
+	unsigned short cmask = 	XCB_CONFIG_WINDOW_WIDTH |
+							XCB_CONFIG_WINDOW_HEIGHT |
+							XCB_CONFIG_WINDOW_BORDER_WIDTH;
+
+	n->x = x;
+	n->y = y;
+	n->width = width;
+	n->height = height;
+	int i = 0;
+
+	if ( ( i = screen->width_in_pixels - n->width - x ) > screen->width_in_pixels ) {
+		n->x = n->x - i;
+	}
+	if ( ( i = screen->height_in_pixels - n->height - y ) > screen->height_in_pixels ) {
+		n->y = n->y - i;
+	}
+	if ( x < 0 ) {
+		n->x = 0;
+	}
+	if ( y < 0 ) {
+		n->y = 0;
+	}
+	unsigned int pv[5] = {
+		x, 
+		y, 
+		width + BORDER_SIZE_LEFT + BORDER_SIZE_RIGHT, 
+		height + BORDER_SIZE_TOP + BORDER_SIZE_BOTTOM, 
+		0
+	};
+	unsigned int cv[3] = {
+		width, 
+		height, 
+		0
+	};
+	
+
+	xcb_configure_window( c, n->parent, pmask, pv );
+	xcb_configure_window( c, n->window, cmask, cv );
+	xcb_flush( c );
+}
+
 void DoCreateNotify( xcb_create_notify_event_t *e ) {
 	unsigned int v[2] = { greyPixel, XCB_EVENT_MASK_EXPOSURE | XCB_EVENT_MASK_SUBSTRUCTURE_NOTIFY | XCB_EVENT_MASK_SUBSTRUCTURE_REDIRECT };
 	client_t *m = firstClient;
 	client_t *n = firstClient;
 	int i = 0;
-	int x, y;
 
 	if ( e->parent != screen->root ) {
 		return;
@@ -71,42 +131,24 @@ void DoCreateNotify( xcb_create_notify_event_t *e ) {
 	}
 
 	n->window = e->window;
-
-	if ( e->override_redirect ) {
-		n->managementState = STATE_UNMANAGED;
-		printf( "New unmanaged window\n" );
-		return;
-	}
+	n->width = e->width;
+	n->height = e->height;
+	n->x = e->x;
+	n->y = e->y;
 
 	n->managementState = STATE_WITHDRAWN;
-	n->width = e->width + 2 + e->border_width * 2;
-	n->height = e->height + 20 + e->border_width * 2;
 
-	x = n->x = e->x - 1;
-	y = n->y = e->y - 20;
-	
-	if ( ( i = screen->width_in_pixels - n->width - x ) > screen->width_in_pixels ) {
-		x = x - i;
+	if ( e->override_redirect ) {
+		n->managementState = STATE_NO_REDIRECT;
+		printf( "New unreparented window\n" );
+		return;
 	}
-	if ( ( i = screen->height_in_pixels - n->height - y ) > screen->height_in_pixels ) {
-		y = y - i;
-	}
-	if ( x < 0 ) {
-		x = 0;
-	}
-	if ( y < 0 ) {
-		y = 0;
-	}
-
-	printf( "x: %d, y: %d w: %d h: %d\n", x, y, n->width, n->height );
-
-
-
-	n->parent = xcb_generate_id(c);
+	n->parent = xcb_generate_id( c );
 	xcb_create_window (		c, XCB_COPY_FROM_PARENT, n->parent, screen->root, 
-							x, y, n->width, n->height, 
+							0, 0, BORDER_SIZE_LEFT + BORDER_SIZE_RIGHT + 1, BORDER_SIZE_TOP + BORDER_SIZE_BOTTOM + 1, 
 							0, XCB_WINDOW_CLASS_INPUT_OUTPUT, screen->root_visual, 
 							XCB_CW_BACK_PIXEL | XCB_CW_EVENT_MASK, v);
+	
 	xcb_reparent_window( c, n->window, n->parent, 1, 19 );
 	xcb_flush( c );
 }
@@ -116,8 +158,9 @@ void DoReparentNotify( xcb_reparent_notify_event_t *e ) {
 
 	for ( ; n != NULL; n = n->nextClient ) {
 		if ( n->window == e->window ) {
-			n->managementState = STATE_MANAGED;
-			printf( "window %x reparented successfully\n", e->window );
+			n->managementState = STATE_REPARENTED;
+			ConfigureClient( n, n->x, n->y, n->width, n->height );
+			printf( "window %x reparented to window %x\n", e->window, e->parent );
 			return;
 		}
 	}
@@ -134,7 +177,8 @@ void DoMapRequest( xcb_map_request_event_t *e ) {
 
 	for ( ; n != NULL; n = n->nextClient ) {
 		if ( n->window == e->window ) {
-			if ( n->managementState == STATE_UNMANAGED ) {
+			n->windowState = STATE_NORMAL;
+			if ( n->managementState == STATE_NO_REDIRECT ) {
 				xcb_map_window( c, e->window );
 				xcb_flush( c );
 				printf( "unmanaged window %x mapped\n", e->window );
@@ -149,6 +193,21 @@ void DoMapRequest( xcb_map_request_event_t *e ) {
 		}
 	}
 	printf("Attempt to map nonexistent window!\n");
+}
+
+void DoConfigureRequest( xcb_configure_request_event_t *e ) {
+	client_t *n = firstClient;
+
+	for ( ; n != NULL; n = n->nextClient ) {
+		if ( n->window == e->window ) {
+			ConfigureClient( n, e->x, e->y, e->width, e->height );
+			return;
+		}
+	}
+}
+
+void DoExpose( xcb_expose_event_t *e ) {
+	printf( "window %x exposed\n", e->window );
 }
 
 int BecomeWM(  ) {
@@ -197,6 +256,8 @@ int main() {
 	signal( SIGTERM, Quit );
 	signal( SIGINT, Quit );
 
+	printf( "%s %s, build %s\n", PROGRAM_NAME, VERSION_STRING, VERSION_BUILDSTR );
+
 	c = xcb_connect( NULL, NULL );
 	if ( xcb_connection_has_error( c ) ) {
 			printf( "Uh oh! It looks like X isn't running.\n" );
@@ -227,6 +288,10 @@ int main() {
 			case XCB_MAP_REQUEST:
 				DoMapRequest( (xcb_map_request_event_t *)e );
 				break;
+			case XCB_CONFIGURE_REQUEST:
+				DoConfigureRequest( (xcb_configure_request_event_t *)e );
+			case XCB_EXPOSE:
+				DoExpose( (xcb_expose_event_t *)e );
 			default:
 				printf( "Warning, unhandled event #%d\n", e->response_type & ~0x80 );
 				break;
